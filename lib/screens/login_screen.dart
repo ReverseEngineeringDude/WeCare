a677ac91780afb94dbbb67ade81532a5d2a1f0bd
ReverseEngineeringDude <praveenmtdarker@gmail.com>
2025-12-21 01:26:01 +0530

    internet permission added v2

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../services/api_service.dart';
 import 'home_screen.dart';
+import 'dart:io';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
